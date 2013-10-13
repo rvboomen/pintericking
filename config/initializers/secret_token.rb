efe62c76230a9ec309e279f2a6b665f2c3e0f624
Rick Vanden Boomen <rvboomen@gmail.com>
2013-10-13 10:53:23 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintericking::Application.config.secret_key_base = '9e847501c5c199f6df57b20a019de6a05b18067e5392a0195d55f6b0582329896cd78b5172f95383c779945881bd6d4423682b6711ae3ca9ec7cad058e018981'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pintericking::Application.config.session_store :cookie_store, key: '_pintericking_session'
